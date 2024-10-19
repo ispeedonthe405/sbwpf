f397cc4ffca16c23ab8f4a19b9920c07e8467c39
ispeedonthe405 <ispeedonthe405@gmail.com>
2024-10-19 06:36:01 -0700

    This interface may not remain but I'm committing it for now

---
+++ Core/IFieldSetter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sbwpf.Core
+{
+    public interface IFieldSetter
+    {
+        public bool SetField<TField>(ref TField field, TField value, string propertyName);
+    }
+}
