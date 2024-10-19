897c8c2951fba73d7ae769c1ac2e088dd28495f0
ispeedonthe405 <ispeedonthe405@gmail.com>
2024-10-19 06:37:11 -0700

    New core lib: MathUtils

---
+++ Core/MathUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace sbwpf.Core
+{
+    public static class MathUtils
+    {
+        public static double NormalizeByte(byte value)
+        {
+            return value / 255.0;
+        }
+    }
+}
