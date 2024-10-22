3a26751567aafc7a51670dee99ccc31080f43cd9
ispeedonthe405 <ispeedonthe405@gmail.com>
2024-10-21 19:29:37 -0700

    Added a tab to demo DataGridEx

---
+++ Test/uc_DataGridEx.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace sbwpf.Test
+{
+    /// <summary>
+    /// Interaction logic for uc_DataGridEx.xaml
+    /// </summary>
+    public partial class uc_DataGridEx : UserControl
+    {
+        public uc_DataGridEx()
+        {
+            InitializeComponent();
+
+            datagrid.DataContext = SampleDataset.Samples;
+        }
+    }
+}
